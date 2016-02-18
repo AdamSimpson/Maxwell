a9510e6dba7b711d531b8c39ce9835dec76ad51c
AdamSimpson <adambsimpson@gmail.com>
2016-02-17 21:27:08 -0500

    Add simple bridge query

--- components/bridges.jsx
+++ components/bridges.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import hue from 'node-hue-api';
 
 export default class Bridges extends React.Component {
   constructor(props) {
     super(props);
+    
+    this.state = {bridges: []}
+
+    this.setBridges  = this.setBridges.bind(this);
+    this.updateBridges = this.updateBridges.bind(this);
+
+    this.updateBridges();
+  }
+
+  setBridges(bridges_payload) {
+    this.setState({bridges: bridges_payload});
+  }
+
+  updateBridges() {
+    return hue.nupnpSearch().then(this.setBridges).done()
   }
 
   render() {
     return(
       <div>
-        bridges
+        Bridges:
+        {JSON.stringify(this.state.bridges)}
       </div>
     );
   }
